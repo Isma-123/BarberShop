458d5984a45c207885f0663dd032d094b33f7ef2
Abdias <felizseguraabdias@gmail.com>
2025-01-20 16:25:47 -0400

    Vistas

---
+++ BarberShop.Persitance.cs/Models/Personal/BarberModels.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace BarberShop.Persitance.cs.Models.Personal
+{
+    public class BarberModels
+    {
+
+        public int BarberID { get; set; }
+
+        public int AppointmentID { get; set; } // clave foranea
+        public string Name { get; set; }
+        public string LicenseNumber { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string Specialty { get; set; } // Puede representarse como un horario estructurado
+        public DateTime register { get; set; }
+        public string? OffDay { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? CreateAt { get; set; }
+
+    }
+}
